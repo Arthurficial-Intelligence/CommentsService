d53cdd272d4ad3c53786992464cc5597ae8dad6d
arthur rankin smith <arthurrankin1992@gmail.com>
2020-05-08 15:26:28 -0500

    Update index.js

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ app.post("/posts/:id/comments", async (req, res) => {
 });
 
 app.post('/events', (req, res) => {
-  console.log('recieved event', req.body.type);
+  console.log('received event', req.body.type);
 
   res.send({})
 })
